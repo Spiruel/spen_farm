5150a453b4837824561c09a567ad21b83123f782
Samuel Bancroft <spiruel@gmail.com>
2023-01-06 16:09:46 +0000

    Update spen_farm_interface_st.py

--- spen_farm_interface_st.py
+++ spen_farm_interface_st.py
@@ -5,6 +5,7 @@ st.title('Spen Farm app')
 imports required libraries
 """
 import ee
+ee.Initialize()
 import eemont
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,7 +19,7 @@ import matplotlib.dates as mdates
 
 import datetime
 
-import geemap
+import geemap.foliumap as geemap
 from most_recent_product import field_closest_product_2_date_pkl
 from read_temp_data import get_temps_4_date_pkl,get_temps_4_date_range_pkl
 from read_rain_data import get_rain_4_date_pkl,get_rain_4_date_range_pkl
